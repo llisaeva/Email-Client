bbf9ac9c362292b3bf5f9aaaa52d082544b82ec9
Lisa <lis.lisaeva@gmail.com>
2020-07-28 23:20:24 -0500

    downloadable attachments, faster loading, folder icons

--- src/main/java/com/lisaeva/email/Launcher.java
+++ src/main/java/com/lisaeva/email/Launcher.java
@@ -2,7 +2,6 @@ package com.lisaeva.email;
 
 import com.lisaeva.email.model.EmailManager;
 import com.lisaeva.email.view.ViewGenerator;
-
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -10,19 +9,13 @@ public class Launcher extends Application {
 	
 	private String account = "";
 	private String password = "";
-	
-
 	public static void main(String... args) {launch(args);}
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-				
+	public void start(Stage primaryStage) throws Exception {			
 		EmailManager em = new EmailManager();
 		em.login(account, password);
-		em.fetchFolders();
-		
-		ViewGenerator.initialize(em);
-		
+		em.fetchFolders();	
+		ViewGenerator.initialize(em);	
 	}
-
 }
